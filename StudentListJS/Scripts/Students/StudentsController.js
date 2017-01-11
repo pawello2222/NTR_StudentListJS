9b9fdcd0a8521c0941a8414120e92dfd19b16a0e
pawello2222 <pawello2222@gmail.com>
2017-01-11 23:27:06 +0100

    Added basic functions

--- StudentListJS/Scripts/Students/StudentsController.js
+++ StudentListJS/Scripts/Students/StudentsController.js
@@ -9,8 +9,45 @@
 
             })
             .error(function (error) {
-                $scope.status = 'Unable to load customer data: ' + error.message;
+                $scope.status = 'Unable to load students: ' + error.message;
 
             });
     }
+
+    $scope.createStudent = function () {
+        var student = {
+            MailID: $scope.mailid,
+            SubscribedDate: new Date()
+        }
+
+        var create = StudentsService.createStudent(student);
+        create.then(function (d) {
+            getStudents();
+        }, function (error) {
+            $scope.status = 'Unable to create student: ' + error.message;
+        })
+    };
+
+    $scope.editStudent = function (ID) {
+        var student = {
+            MailID: $scope.mailid,
+            SubscribedDate: new Date()
+        }
+
+        var edit = StudentsService.editStudent(ID, student);
+        edit.then(function (d) {
+            getStudents();
+        }, function (error) {
+            $scope.status = 'Unable to edit student: ' + error.message;
+        })
+    };
+
+    $scope.deleteStudent = function (ID) {
+        var dlt = StudentsService.deleteStudent(ID);
+        dlt.then(function (d) {
+            getStudents();
+        }, function (error) {
+            $scope.status = 'Unable to delete student: ' + error.message;
+        })
+    };
 });
--- StudentListJS/Scripts/Students/StudentsService.js
+++ StudentListJS/Scripts/Students/StudentsService.js
@@ -2,8 +2,33 @@
 
     var urlBase = 'http://localhost:62461/api';
     var StudentsService = {};
+
     StudentsService.getStudents = function () {
         return $http.get(urlBase + '/Students');
+    };
+
+    StudentsService.createStudent = function (student) {
+        return $http({
+            method: 'post',
+            data: student,
+            url: urlBase + '/Students'
+        });
+    };
+
+    StudentsService.editStudent = function (ID, student) {
+        return $http({
+            method: 'put',
+            data: [ ID, student ],
+            url: urlBase + '/Students/' + ID
+        });
+    };
+
+    StudentsService.deleteStudent = function (ID) {
+        return $http({
+            method: 'delete',
+            data: ID,
+            url: urlBase + '/Students/' + ID
+        });
     };
 
     return StudentsService;
